dbcde4e16264f76c3700dbf0279ed7700e6e2e1c
amit-vishwa <amitvishwa789@gmail.com>
2025-01-09 19:57:53 +0530

    completed spring application context

---
+++ src/main/java/com/spring/tutorial/applicationcontext/DemoApplication.java
@@ -0,0 +1,52 @@
+package com.spring.tutorial.applicationcontext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+// Class
+ @SpringBootApplication
+public class DemoApplication {
+
+    // Main driver method
+    public static void main(String[] args) {
+
+        usingSpringApplication(args);
+        usingAnnotationConfigApplicationContext();
+        usingFileSystemXmlApplicationContext();
+        usingClassPathXmlApplicationContext();
+
+    }
+
+    static void usingSpringApplication(String[] args){
+        // Student bean is fetched by SpringBootApplication using Annotation
+        ApplicationContext context = SpringApplication.run(DemoApplication.class, args);
+        Student student = context.getBean(Student.class);
+        System.out.println("Using spring application: " + student);
+    }
+
+    static void usingAnnotationConfigApplicationContext(){
+        // Student bean is fetched from config file
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        Student student = context.getBean(Student.class);
+        System.out.println("Using annotation config application context: " + student);
+    }
+
+    static void usingFileSystemXmlApplicationContext(){
+        String path = "src/main/resources/application-context-demo.xml";
+        ApplicationContext context = new FileSystemXmlApplicationContext(path);
+        Student student = context.getBean("student", Student.class);
+        System.out.println("Using file system xml application context: " + student);
+    }
+
+    static void usingClassPathXmlApplicationContext(){
+        String path = "application-context-demo.xml";
+        ApplicationContext context = new ClassPathXmlApplicationContext(path);
+        Student student = context.getBean("student", Student.class);
+        System.out.println("Using class path xml application context: " + student);
+    }
+
+}
