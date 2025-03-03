2a5934cc07f8753d944649eab56c39468ea4e455
amit-vishwa <amitvishwa789@gmail.com>
2025-03-03 20:56:47 +0530

    threading model and common ForkJoinPool from section 9

--- parallel-asynchronous-using-java-main/src/test/java/com/learnjava/service/CheckoutServiceTest.java
+++ parallel-asynchronous-using-java-main/src/test/java/com/learnjava/service/CheckoutServiceTest.java
@@ -6,6 +6,8 @@ import com.learnjava.domain.checkout.CheckoutStatus;
 import com.learnjava.util.DataSet;
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.ForkJoinPool;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CheckoutServiceTest {
@@ -28,6 +30,24 @@ class CheckoutServiceTest {
     void checkout_25_items() {
         // given
         Cart cart = DataSet.createCart(25);
+        // when
+        CheckoutResponse checkoutResponse = checkoutService.checkout(cart);
+        // then
+        assertEquals(CheckoutStatus.FAILURE,checkoutResponse.getCheckoutStatus());
+    }
+
+    @Test
+    void parallelism() {
+        // one core is used by thread CheckoutServiceTest, hence output is 7 off 8
+        System.out.println("Parallelism: " + ForkJoinPool.getCommonPoolParallelism());
+    }
+
+    @Test
+    void modify_parallelism() {
+        // given
+        // we can set worker threads count using below
+//        System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", "100");
+        Cart cart = DataSet.createCart(100);
         // when
         CheckoutResponse checkoutResponse = checkoutService.checkout(cart);
         // then
